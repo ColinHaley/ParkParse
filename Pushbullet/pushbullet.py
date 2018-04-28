eb8ec9f584a8d78bde4c30ce272e7992cb06b149
Colin Haley <admin@colinhaley.net>
2018-04-28 10:20:14 -0600

    scoping is hard

--- Pushbullet/pushbullet.py
+++ Pushbullet/pushbullet.py
@@ -3,12 +3,6 @@ import datetime
 import os
 import requests
 
-API_ROOT="https://api.pushbullet.com/v2/"
-CLIENT_ID = None
-CLIENT_SECRET = None
-AUTH_CODE = None
-ACCESS_TOKEN = None
-
 for line in open('config.env'):
     if line[0] == '#':
         continue
@@ -24,61 +18,70 @@ pushbullet_endpoints = {
 }
 
 class Config(object):
-    if os.environ.get('CLIENT_ID'):
-        CLIENT_ID = os.environ.get('CLIENT_ID')
-        print(CLIENT_ID)
-    else:
-        CLIENT_ID = 'CLIENT_ID_NOT_SPECIFIED'
-        print('CLIENT_ID needs to be specified in a config.env file.')
-
-    if os.environ.get('CLIENT_SECRET'):
-        CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-        print(CLIENT_SECRET)
-    else:
-        CLIENT_SECRET = 'CLIENT_SECRET_NOT_SPECIFIED'
-        print('CLIENT_SECRET needs to be specified in a config.env file.')
-
-    if os.environ.get('AUTH_CODE'):
-        AUTH_CODE = os.environ.get('AUTH_CODE')
-        print(AUTH_CODE)
-    else:
-        AUTH_CODE = 'AUTH_CODE_NOT_SPECIFIED'
-        print('AUTH_CODE needs to be specified in a config.env file.')
-
-    if os.environ.get('PB_ACCESS_TOKEN'):
-        ACCESS_TOKEN = os.environ.get('PB_ACCESS_TOKEN')
-        print(ACCESS_TOKEN)
-    else:
-        ACCESS_TOKEN = 'ACCESS_TOKEN_NOT_SPECIFIED'
-        print('ACCESS_TOKEN needs to be specified in a config.env file.')
+    API_ROOT = "https://api.pushbullet.com/v2/"
+    CLIENT_ID = None
+    CLIENT_SECRET = None
+    AUTH_CODE = None
+    ACCESS_TOKEN = None
 
     def __init__(self,__verbose__ = False):
         print('New Run:  {0}'.format(datetime.datetime.now()))
+        if os.environ.get('CLIENT_ID'):
+            CLIENT_ID = os.environ.get('CLIENT_ID')
+            print(CLIENT_ID)
+        else:
+            CLIENT_ID = 'CLIENT_ID_NOT_SPECIFIED'
+            print('CLIENT_ID needs to be specified in a config.env file.')
 
-    def query_endpoint(sel  f):
+        if os.environ.get('CLIENT_SECRET'):
+            CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+            print(CLIENT_SECRET)
+        else:
+            CLIENT_SECRET = 'CLIENT_SECRET_NOT_SPECIFIED'
+            print('CLIENT_SECRET needs to be specified in a config.env file.')
+
+        if os.environ.get('AUTH_CODE'):
+            AUTH_CODE = os.environ.get('AUTH_CODE')
+            print(AUTH_CODE)
+        else:
+            AUTH_CODE = 'AUTH_CODE_NOT_SPECIFIED'
+            print('AUTH_CODE needs to be specified in a config.env file.')
+
+        if os.environ.get('PB_ACCESS_TOKEN'):
+            ACCESS_TOKEN = os.environ.get('PB_ACCESS_TOKEN')
+            print(ACCESS_TOKEN)
+        else:
+            ACCESS_TOKEN = 'ACCESS_TOKEN_NOT_SPECIFIED'
+            print('ACCESS_TOKEN needs to be specified in a config.env file.')
+
+
+
+    def query_endpoint(self):
         print('query ep')
 
     def get_bearer_token(query_endpoint):
         endpoint = API_ROOT + pushbullet_endpoints['bearer token']
-        headers = {
-            "Access-Token":ACCESS_TOKEN,
-            "Content-Type":"application/json"
-        }
-        dataset = {
-            'client_id' : CLIENT_ID,
-            'client_sercret' : CLIENT_SECRET,
-            'code' : AUTH_CODE,
-            'grant_type' : 'authorization_code'
-        }
+        headers = {}
+
+        headers["Access-Token"] = ACCESS_TOKEN
+        headers["Content-Type"] = "application/json"
+        dataset = {}
+
+        dataset['client_id'] = CLIENT_ID
+        dataset['client_sercret'] = CLIENT_SECRET
+        dataset['code'] = AUTH_CODE
+        dataset['grant_type'] = 'authorization_code'
+
         response = requests.post(endpoint,headers=headers,data=dataset)
         print (response.content)
         print("\n")
         print (endpoint)
         print('\n')
+        print(ACCESS_TOKEN)
+        print(json.dumps(headers))
+        print('\n')
         print(json.dumps(dataset))
 
-
-    @staticmethod
     def get_devices(query_endpoint):
         headers = {'Access-Token':"a"}
         response = requests.get(API_ROOT + pushbullet_endpoints['user info'])
