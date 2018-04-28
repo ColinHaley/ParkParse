56935ece86ee4ad90e6e138d886f1bdd1dd60fd8
Colin Haley <admin@colinhaley.net>
2018-04-28 15:14:11 -0600

    Scope up everything from self to super

--- Pushbullet/pushbullet.py
+++ Pushbullet/pushbullet.py
@@ -5,7 +5,7 @@ import requests
 
 # Override and global variables
 __bearer_token_override__ = True
-__cmh__ = None
+__cmh__ = True
 # End override and global variables
 
 for line in open('config.env'):
@@ -31,14 +31,14 @@ class Config(object):
     AUTH_CODE = None
     ACCESS_TOKEN = None
 
-    # The Bearer Token should be retrieved live via the Config.get_bearer_token() call
+    # The Bearer Token should be retrieved live via the Config.update_bearer_token() call
     # if necessary, the BEARER_TOKEN config line can be written and a global override of
     # __bearer_token_override__ can be set to True to read from the config.
     BEARER_TOKEN = None
 
     # These objects will be set and referenced by other methods within the class
     PB_USER_IDEN = None
-    PB_DEVICES = None
+    PB_DEVICES = {}
 
     def __init__(self):
         print('New Run:  {0}'.format(datetime.datetime.now()))
@@ -68,32 +68,33 @@ class Config(object):
         if (__bearer_token_override__):
             self.BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
 
-    def query_endpoint():
-        print('query ep')
+        self.update_bearer_token()
+        self.update_user_iden()
+        self.update_devices()
 
-    def get_bearer_token(self):
-        if(__bearer_token_override__ and self.BEARER_TOKEN):
+    def update_bearer_token(super):
+        if(__bearer_token_override__ and super.BEARER_TOKEN):
             print("Bearer token override enabled and Bearer token already properly set.")
         else:
-            endpoint = self.API_ROOT + pushbullet_endpoints['bearer token']
+            endpoint = super.API_ROOT + pushbullet_endpoints['bearer token']
             headers = {}
 
-            headers["Access-Token"] = self.ACCESS_TOKEN
+            headers["Access-Token"] = super.ACCESS_TOKEN
             headers["Content-Type"] = "application/json"
             dataset = {}
 
-            dataset['client_id'] = self.CLIENT_ID
-            dataset['client_sercret'] = self.CLIENT_SECRET
-            dataset['code'] = self.AUTH_CODE
+            dataset['client_id'] = super.CLIENT_ID
+            dataset['client_sercret'] = super.CLIENT_SECRET
+            dataset['code'] = super.AUTH_CODE
             dataset['grant_type'] = 'authorization_code'
 
             response = requests.post(endpoint,headers=headers,data=dataset)
             if (response.status_code == 200):
-                self.BEARER_TOKEN = response.content['Access-Token']
+                super.BEARER_TOKEN = response.content['Access-Token']
                 print ("Bearer token successfully retrieved")
             else:
                 print (response.content)
-                self.BEARER_TOKEN = "BEARER_TOKEN_NOT_SET"
+                super.BEARER_TOKEN = "BEARER_TOKEN_NOT_SET"
                 if (__cmh__):
                     print("\n")
                     print(json.dumps(dataset))
@@ -103,7 +104,7 @@ class Config(object):
                     print(endpoint)
                     print(response.status_code)
 
-    def get_devices(self):
+    def update_user_iden(self):
         headers = {'Access-Token':self.BEARER_TOKEN}
         response = requests.get(self.API_ROOT + pushbullet_endpoints['user info'],headers=headers)
         if (response.status_code == 200):
@@ -112,6 +113,25 @@ class Config(object):
             print("Error acquiring user iden key from pushbullet")
             self.PB_USER_IDEN = "PB_USER_IDEN_NOT_SET"
 
+    def get_user_iden(super):
+        if (__cmh__):
+            print super.PB_USER_IDEN
+        return super.PB_USER_IDEN
+
+    def update_devices(super):
+        headers = {'Access-Token':super.BEARER_TOKEN}
+        response = requests.get(super.API_ROOT + pushbullet_endpoints['list devices'],headers=headers)
+        if (response.status_code == 200):
+            for device in json.loads(response.content)['devices']:
+                super.PB_DEVICES[device['type']] = {
+                    "nickname": device['nickname'],
+                    "iden" : device['iden']
+                }
+        else:
+            print("Error acquiring user iden key from pushbullet")
+            super.PB_DEVICES= "PB_USER_IDEN_NOT_SET"
+
+        print json.dumps(super.PB_DEVICES)
     #TODO: Write debug method for Config() class
 
-test=Config().get_devices()
+test=Config().get_user_iden()
