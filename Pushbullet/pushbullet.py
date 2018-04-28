8b724a147798372e285414ba4113313c8e81bb8a
Colin Haley <admin@colinhaley.net>
2018-04-28 13:15:49 -0600

    Add a debug route by variable __cmh__ true/false, and add Bearer Token override retrieval if specified in the environment file

--- Pushbullet/pushbullet.py
+++ Pushbullet/pushbullet.py
@@ -3,6 +3,8 @@ import datetime
 import os
 import requests
 
+__bearer_token_override__ = True
+
 for line in open('config.env'):
     if line[0] == '#':
         continue
@@ -17,73 +19,79 @@ pushbullet_endpoints = {
     "bearer token":"oauth2/token"
 }
 
-class Config(object):
+class Config():
     API_ROOT = "https://api.pushbullet.com/v2/"
     CLIENT_ID = None
     CLIENT_SECRET = None
     AUTH_CODE = None
     ACCESS_TOKEN = None
+    BEARER_TOKEN = None
 
     def __init__(self,__verbose__ = False):
         print('New Run:  {0}'.format(datetime.datetime.now()))
         if os.environ.get('CLIENT_ID'):
-            CLIENT_ID = os.environ.get('CLIENT_ID')
-            print(CLIENT_ID)
+            self.CLIENT_ID = os.environ.get('CLIENT_ID')
         else:
-            CLIENT_ID = 'CLIENT_ID_NOT_SPECIFIED'
+            self.CLIENT_ID = 'CLIENT_ID_NOT_SPECIFIED'
             print('CLIENT_ID needs to be specified in a config.env file.')
 
         if os.environ.get('CLIENT_SECRET'):
-            CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-            print(CLIENT_SECRET)
+            self.CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
         else:
-            CLIENT_SECRET = 'CLIENT_SECRET_NOT_SPECIFIED'
+            self.CLIENT_SECRET = 'CLIENT_SECRET_NOT_SPECIFIED'
             print('CLIENT_SECRET needs to be specified in a config.env file.')
-
         if os.environ.get('AUTH_CODE'):
-            AUTH_CODE = os.environ.get('AUTH_CODE')
-            print(AUTH_CODE)
+            self.AUTH_CODE = os.environ.get('AUTH_CODE')
         else:
-            AUTH_CODE = 'AUTH_CODE_NOT_SPECIFIED'
+            self.AUTH_CODE = 'AUTH_CODE_NOT_SPECIFIED'
             print('AUTH_CODE needs to be specified in a config.env file.')
 
         if os.environ.get('PB_ACCESS_TOKEN'):
-            ACCESS_TOKEN = os.environ.get('PB_ACCESS_TOKEN')
-            print(ACCESS_TOKEN)
+            self.ACCESS_TOKEN = os.environ.get('PB_ACCESS_TOKEN')
         else:
-            ACCESS_TOKEN = 'ACCESS_TOKEN_NOT_SPECIFIED'
+            self.ACCESS_TOKEN = 'ACCESS_TOKEN_NOT_SPECIFIED'
             print('ACCESS_TOKEN needs to be specified in a config.env file.')
 
+        if (__bearer_token_override__):
+            self.BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
 
-
-    def query_endpoint(self):
+    def query_endpoint():
         print('query ep')
 
-    def get_bearer_token(query_endpoint):
-        endpoint = API_ROOT + pushbullet_endpoints['bearer token']
-        headers = {}
+    def get_bearer_token(self):
+        if(__bearer_token_override__ and self.BEARER_TOKEN):
+            print("Bearer token override enabled and Bearer token already properly set.")
+        else:
+            endpoint = self.API_ROOT + pushbullet_endpoints['bearer token']
+            headers = {}
 
-        headers["Access-Token"] = ACCESS_TOKEN
-        headers["Content-Type"] = "application/json"
-        dataset = {}
+            headers["Access-Token"] = self.ACCESS_TOKEN
+            headers["Content-Type"] = "application/json"
+            dataset = {}
 
-        dataset['client_id'] = CLIENT_ID
-        dataset['client_sercret'] = CLIENT_SECRET
-        dataset['code'] = AUTH_CODE
-        dataset['grant_type'] = 'authorization_code'
+            dataset['client_id'] = self.CLIENT_ID
+            dataset['client_sercret'] = self.CLIENT_SECRET
+            dataset['code'] = self.AUTH_CODE
+            dataset['grant_type'] = 'authorization_code'
 
-        response = requests.post(endpoint,headers=headers,data=dataset)
-        print (response.content)
-        print("\n")
-        print (endpoint)
-        print('\n')
-        print(ACCESS_TOKEN)
-        print(json.dumps(headers))
-        print('\n')
-        print(json.dumps(dataset))
+            response = requests.post(endpoint,headers=headers,data=dataset)
+            if (response.status_code == 200):
+                self.BEARER_TOKEN = response.content['Access-Token']
+                print ("Bearer token successfully retrieved")
+            else:
+                print (response.content)
+                self.BEARER_TOKEN = "BEARER_TOKEN_NOT_SET"
+                if (__cmh__):
+                    print("\n")
+                    print(json.dumps(dataset))
+                    print("\n")
+                    print(json.dumps(headers))
+                    print("\n")
+                    print(endpoint)
+                    print(response.status_code)
 
     def get_devices(query_endpoint):
         headers = {'Access-Token':"a"}
-        response = requests.get(API_ROOT + pushbullet_endpoints['user info'])
+        response = requests.get(self.API_ROOT + pushbullet_endpoints['user info'])
 
 test=Config().get_bearer_token()
